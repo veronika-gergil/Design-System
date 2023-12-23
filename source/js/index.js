4153ff2eddd68938a25bd994377c5e496a18eaed
Gyallarhorn <a.r.b.32@mail.ru>
2023-12-23 18:23:05 +0300

    Modal section

--- source/js/index.js
+++ source/js/index.js
@@ -1,1 +1,17 @@
 import './utils/changeColor';
+
+const form = document.querySelector('.form-area')
+
+console.log(form)
+
+const regExp = {
+  name: /^[A-Za-zА-Яа-я]+$/u,
+  email: /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/
+}
+
+form.addEventListener('focusout', (e) => {
+  if (e.target.classList.contains('form-area__input')) {
+    const check = regExp[e.target.name].test(e.target.value)
+    check ? e.target.classList.add('form-area__input--error') : e.target.classList.remove('form-area__input--error')
+  }
+})
