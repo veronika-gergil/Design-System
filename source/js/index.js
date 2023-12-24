624068aa6d55694b30ab86ca21f89eeae78b599a
Gyallarhorn <a.r.b.32@mail.ru>
2023-12-24 11:41:57 +0300

    Form validation

--- source/js/index.js
+++ source/js/index.js
@@ -1,17 +1,35 @@
 import './utils/changeColor';
 
-const form = document.querySelector('.form-area')
+const form = document.querySelector('.form-area');
+const formInputs = document.querySelectorAll('.form-area__input');
+const comment = document.querySelector('.form-area__comment');
+const submitButton = document.querySelector('.form-area__button');
+const modal = document.querySelector('.modal');
+const closeButton = document.querySelector('.modal__body-button');
 
-console.log(form)
+const allInputs = Array.from(formInputs);
+allInputs.push(comment);
 
-const regExp = {
-  name: /^[A-Za-zА-Яа-я]+$/u,
-  email: /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/
-}
+const debounce = (callback, timeoutDelay) => {
+  let timeoutId;
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+};
 
-form.addEventListener('focusout', (e) => {
-  if (e.target.classList.contains('form-area__input')) {
-    const check = regExp[e.target.name].test(e.target.value)
-    check ? e.target.classList.add('form-area__input--error') : e.target.classList.remove('form-area__input--error')
-  }
-})
+const checkEmpty = () => allInputs.every((elem) => elem.value.trim() !== '');
+
+form.addEventListener('input', debounce(() => {
+  const isEmpty = checkEmpty();
+  submitButton.disabled = !isEmpty;
+}, 500));
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  modal.classList.add('modal--active');
+});
+
+closeButton.addEventListener('click', () => {
+  modal.classList.remove('modal--active');
+});
