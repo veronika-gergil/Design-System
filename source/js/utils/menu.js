050cdac82bccb13dc5c692fa2926e04e5b5d474f
Gyallarhorn <a.r.b.32@mail.ru>
2024-01-06 20:35:19 +0300

    fix menu error and lint observer.js

--- source/js/utils/menu.js
+++ source/js/utils/menu.js
@@ -1,11 +1,10 @@
 const currentPath = window.location.pathname;
 const currentPage = currentPath.replace(/\/([^\/.]+)\.html$/, '$1');
 
-if (currentPage !== 'index') {
+if (currentPage !== '/' && currentPage !== 'index') {
   const navList = document.querySelector('.nav-list');
   navList.querySelector(`[data-code=${currentPage}]`).classList.add('active');
   navList.querySelector(`[data-active=${currentPage}]`).classList.add('active');
-
 
   navList.addEventListener('click', (e) => {
     if (!(e.target && e.target.classList.contains('nav-list__button'))) {
--- source/js/utils/observer.js
+++ source/js/utils/observer.js
@@ -1,23 +1,21 @@
-const scrollSections = document.querySelectorAll('.observed-section')
-const menuItems = document.querySelectorAll('.nav-list__accordion.active .inner-nav__link')
+const scrollSections = document.querySelectorAll('.observed-section');
+const menuItems = document.querySelectorAll('.nav-list__accordion.active .inner-nav__link');
 
-const removeClass = (nodeList, className) => nodeList.forEach((elem) => elem.classList.remove(className))
+const removeClass = (nodeList, className) => nodeList.forEach((elem) => elem.classList.remove(className));
 
-const findElemByDataId = (nodeList, dataId, value) => Array.from(nodeList).find((elem) => elem.dataset[dataId] === value)
-
+const findElemByDataId = (nodeList, dataId, value) => Array.from(nodeList).find((elem) => elem.dataset[dataId] === value);
 
 if (scrollSections.length > 0) {
   const scrollObserver = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
-        removeClass(menuItems, 'active')
-        const targetElement = findElemByDataId(menuItems, 'link', entry.target.id)
-        targetElement.classList.add('active')
+        removeClass(menuItems, 'active');
+        const targetElement = findElemByDataId(menuItems, 'link', entry.target.id);
+        targetElement.classList.add('active');
       }
-    })
+    });
   }, {
-    threshold: 0.5
-  })
-
-  scrollSections.forEach((section) => scrollObserver.observe(section))
+    threshold: 0.5,
+  });
+  scrollSections.forEach((section) => scrollObserver.observe(section));
 }
